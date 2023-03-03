2a7439038fe8e466621cd5fe443165b1d2748576
rnguy774 <rnguy774@mtroyal.ca>
2023-03-03 13:52:02 -0700

    yurr

--- Assignment 2 - Skeleton/src/mru/toys/application/AppDriver.java
+++ Assignment 2 - Skeleton/src/mru/toys/application/AppDriver.java
@@ -1,10 +1,12 @@
 package mru.toys.application;
 
+import mru.toys.controller.ToyManager;
+
 public class AppDriver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new ToyManager();
 	}
 
 }
--- Assignment 2 - Skeleton/src/mru/toys/controller/ToyManager.java
+++ Assignment 2 - Skeleton/src/mru/toys/controller/ToyManager.java
@@ -1,5 +1,45 @@
 package mru.toys.controller;
 
-public class ToyManager {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+import mru.toys.model.Toys;
+import mru.toys.view.AppMenu;
+
+public class ToyManager {
+	final String FILE_PATH = "res/toys.txt"; 
+	ArrayList<Toys> toy;
+	AppMenu appMen;
+	
+	public ToyManager() {
+		toy = new ArrayList<>();
+		appMen = new AppMenu();
+//		loadData();
+//		launchApplication();
+	}
+	
+	public void launchApplication() throws IOException {
+		boolean flag = true;
+		int option;
+		
+		while(flag) {
+			option = appMen.showMainMenu();
+			
+			switch(option) {
+			case 1:
+//				search();
+				break;
+			case 2:
+//				addToy();
+				break;
+			case 3:
+//				removeToy();
+				break;
+			case 4:
+//				save();
+			}
+		}
+		
+	}
 }
