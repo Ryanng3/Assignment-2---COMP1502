bd63d8a3126875a9f954a802d0a3d77475b4add3
jjoeb099 <jjoeb099@mtroyal.ca>
2023-03-06 16:34:52 -0700

    hi

--- Assignment 2 - Skeleton/src/mru/toys/model/BoardGames.java
+++ Assignment 2 - Skeleton/src/mru/toys/model/BoardGames.java
@@ -5,10 +5,13 @@ public class BoardGames extends Toys {
 	public int minPlayers;
 	public int maxPlayers;
 	public String[] designers;
+	public String numOfPlayers;
 	
+	
+	//number of players into one string
 	
 	public BoardGames(Long serialNumber, String name, String brand, double price, int availableCount,
-			int ageAppropriate, int minPlayers, int maxPlayers, String designers) {
+			int ageAppropriate, int minPlayers, int maxPlayers, String designers, String numOfPlayers) {
 		super(serialNumber, name, brand, price, availableCount, ageAppropriate);;
 		this.minPlayers = minPlayers;
 		this.maxPlayers = maxPlayers;
@@ -25,6 +28,14 @@ public class BoardGames extends Toys {
 	
 	public String[] getDesigners() {
 		return designers;
+	}
+	
+	public String print() {
+		return serialNumber + name + brand + price + availableCount + ageAppropriate + numOfPlayers + designers;
+	}
+	
+	public String toString() {
+		return "Category: Board Game," + "Serial Number: " + serialNumber + "," + "Name: " + name + "Brand: " + brand + "Price: " + price + "Available Count: " + availableCount + "Age Appropriate: " + ageAppropriate + "Number of Players: " + numOfPlayers + "Designers: " + designers;
 	}
 	
 	
